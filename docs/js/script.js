068cd5bc7b17bdb87e7a9362cdfa26bb01d56fd5
gusper01 <gusper01@gmail.com>
2024-08-14 20:23:43 +0000

    Update script.js

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,1 +1,29 @@
+// Animación de la flecha punteada
+gsap.to("#line1", {
+    duration: 1,
+    strokeDashoffset: -20,
+    repeat: -1,
+    ease: "none"
+});
+
+// Animación de los nodos al llegar el flujo de datos
+gsap.to("#node1", {
+    duration: 2,
+    backgroundColor: "#ff5722",
+    scale: 1.2,
+    repeat: -1,
+    yoyo: true,
+    ease: "power1.inOut",
+    delay: 0.5
+});
+
+gsap.to("#node2", {
+    duration: 2,
+    backgroundColor: "#ff5722",
+    scale: 1.2,
+    repeat: -1,
+    yoyo: true,
+    ease: "power1.inOut",
+    delay: 1
+});
 
